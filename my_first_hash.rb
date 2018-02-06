e5484577876cb62ddd23891b2a5588d7acd130d7
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-02-06 05:03:37 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,8 +20,8 @@ end
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   
-  #your code here
-  #remember to return the shipping_manifest hash
+  shipping_manifest["muskets"] = 2 
+  shipping_manifest["gun powder"] = 4 
 
 end
 
